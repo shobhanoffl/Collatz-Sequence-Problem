d33ce0d1f012e7e305dd788eac730c093dc187f2
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-15 03:27:26 +0530

    Initial commit done

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        while(n!=1){
+            if(n%2==0){
+                n/=2;
+            }else{
+                n=(3*n)+1;
+            }
+            System.out.println(n);
+        }
+
+    }
+}
